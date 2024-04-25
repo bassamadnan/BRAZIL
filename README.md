17095368c58eb2dbdb37a20ef01a80a39d4615da
bassamadnan <bassamadnan2012@gmail.com>
2024-04-26 02:25:22 +0530

    add boilerplate files for git

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 2. Create a virtual env: `python3 -m venv venv`
 3. Activate the virtual environment: `source venv/bin/activate`
 4. Install the dependencies: `pip install -r requirements.txt`
-5. ???
+5. ??? (run main.py)
--- app/shapes/shape.py
+++ app/shapes/shape.py
@@ -14,24 +14,6 @@ class Shape(ABC):
     def boundingRect(self):
         pass
 
-class Line:
-    def __init__(self, start_point, end_point):
-        self.start_point = QPointF(start_point)
-        self.end_point = QPointF(end_point)
-
-    def boundingRect(self):
-        x1, y1 = self.start_point.x(), self.start_point.y()
-        x2, y2 = self.end_point.x(), self.end_point.y()
-        left = min(x1, x2)
-        top = min(y1, y2)
-        right = max(x1, x2)
-        bottom = max(y1, y2)
-        return QRectF(left, top, right - left, bottom - top)
-
-    def length(self):
-        dx = self.end_point.x() - self.start_point.x()
-        dy = self.end_point.y() - self.start_point.y()
-        return (dx ** 2 + dy ** 2) ** 0.5
 
 class Rectangle(Shape):
     def __init__(self, top_left, bottom_right):
