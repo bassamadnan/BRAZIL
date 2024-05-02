8bbb6aaffc48693bb9217e0a1c475fd9388a8c0d
ihsingh2 <ihsingh2@gmail.com>
2024-05-02 15:32:38 +0530

    add color edit option for rectangle

--- app/draw/shapeEdit.py
+++ app/draw/shapeEdit.py
@@ -9,54 +9,34 @@ class ShapeEditDialog(QDialog):
         super().__init__(parent)
         self.setWindowTitle("Edit Shape")
         self.shape = shape
-
         layout = QVBoxLayout()
 
-        if isinstance(shape, Line):
-            start_point_label = QLabel("Start Point:")
-            self.start_point_x_input = QLineEdit(str(shape.start_point.x()))
-            self.start_point_y_input = QLineEdit(str(shape.start_point.y()))
-            start_point_layout = QVBoxLayout()
-            start_point_layout.addWidget(self.start_point_x_input)
-            start_point_layout.addWidget(self.start_point_y_input)
-            layout.addWidget(start_point_label)
-            layout.addLayout(start_point_layout)
+        start_point_label = QLabel("Start Point:")
+        self.start_point_x_input = QLineEdit(str(shape.start_point.x()))
+        self.start_point_y_input = QLineEdit(str(shape.start_point.y()))
+        start_point_layout = QVBoxLayout()
+        start_point_layout.addWidget(self.start_point_x_input)
+        start_point_layout.addWidget(self.start_point_y_input)
+        layout.addWidget(start_point_label)
+        layout.addLayout(start_point_layout)
 
-            end_point_label = QLabel("End Point:")
-            self.end_point_x_input = QLineEdit(str(shape.end_point.x()))
-            self.end_point_y_input = QLineEdit(str(shape.end_point.y()))
-            end_point_layout = QVBoxLayout()
-            end_point_layout.addWidget(self.end_point_x_input)
-            end_point_layout.addWidget(self.end_point_y_input)
-            layout.addWidget(end_point_label)
-            layout.addLayout(end_point_layout)
+        end_point_label = QLabel("End Point:")
+        self.end_point_x_input = QLineEdit(str(shape.end_point.x()))
+        self.end_point_y_input = QLineEdit(str(shape.end_point.y()))
+        end_point_layout = QVBoxLayout()
+        end_point_layout.addWidget(self.end_point_x_input)
+        end_point_layout.addWidget(self.end_point_y_input)
+        layout.addWidget(end_point_label)
+        layout.addLayout(end_point_layout)
 
-            color_label = QLabel("Line Color:")
-            self.color_combo = QComboBox()
-            self.color_combo.addItems(["Black", "Red", "Blue", "Green"])
-            self.color_combo.setCurrentText(shape.color.name())
-            layout.addWidget(color_label)
-            layout.addWidget(self.color_combo)
+        color_label = QLabel("Line Color:")
+        self.color_combo = QComboBox()
+        self.color_combo.addItems(["Black", "Red", "Blue", "Green"])
+        self.color_combo.setCurrentText(shape.color.name())
+        layout.addWidget(color_label)
+        layout.addWidget(self.color_combo)
 
-        elif isinstance(shape, Rectangle):
-            start_point_label = QLabel("Start Point:")
-            self.start_point_x_input = QLineEdit(str(shape.start_point.x()))
-            self.start_point_y_input = QLineEdit(str(shape.start_point.y()))
-            start_point_layout = QVBoxLayout()
-            start_point_layout.addWidget(self.start_point_x_input)
-            start_point_layout.addWidget(self.start_point_y_input)
-            layout.addWidget(start_point_label)
-            layout.addLayout(start_point_layout)
-
-            end_point_label = QLabel("End Point:")
-            self.end_point_x_input = QLineEdit(str(shape.end_point.x()))
-            self.end_point_y_input = QLineEdit(str(shape.end_point.y()))
-            end_point_layout = QVBoxLayout()
-            end_point_layout.addWidget(self.end_point_x_input)
-            end_point_layout.addWidget(self.end_point_y_input)
-            layout.addWidget(end_point_label)
-            layout.addLayout(end_point_layout)
-
+        if isinstance(shape, Rectangle):
             corner_type_label = QLabel("Corner Type:")
             self.corner_type_combo = QComboBox()
             self.corner_type_combo.addItems(["Rounded", "Perpendicular"])
@@ -71,17 +51,11 @@ class ShapeEditDialog(QDialog):
         self.setLayout(layout)
 
     def save_changes(self):
-        if isinstance(self.shape, Line):
-            start_point = QPointF(float(self.start_point_x_input.text()), float(self.start_point_y_input.text()))
-            end_point = QPointF(float(self.end_point_x_input.text()), float(self.end_point_y_input.text()))
-            self.shape.start_point = start_point
-            self.shape.end_point = end_point
-            self.shape.color = QColor(self.color_combo.currentText())
-        elif isinstance(self.shape, Rectangle):
-            start_point = QPointF(float(self.start_point_x_input.text()), float(self.start_point_y_input.text()))
-            end_point = QPointF(float(self.end_point_x_input.text()), float(self.end_point_y_input.text()))
-            self.shape.start_point = start_point
-            self.shape.end_point = end_point
+        start_point = QPointF(float(self.start_point_x_input.text()), float(self.start_point_y_input.text()))
+        end_point = QPointF(float(self.end_point_x_input.text()), float(self.end_point_y_input.text()))
+        self.shape.start_point = start_point
+        self.shape.end_point = end_point
+        self.shape.color = QColor(self.color_combo.currentText())
+        if isinstance(self.shape, Rectangle):
             self.shape.rounded = (self.corner_type_combo.currentText() == "Rounded")
-
         self.accept()
